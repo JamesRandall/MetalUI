63f82aece8f974276320ba50e58fdb1931f74a13
James Randall <james@accidentalfish.com>
2024-12-11 07:08:08 +0000

    Code cleanup

--- Sources/MetalUI/internal/runtime/tree/RenderTree.swift
+++ Sources/MetalUI/internal/runtime/tree/RenderTree.swift
@@ -18,6 +18,7 @@ private func renderViewProperties<V: View>(_ view: V, builder: GuiViewBuilder, p
     }
     let requestedSize = getRequestedSize(view, builder: builder, maxWidth: maxWidth, maxHeight: maxHeight)
     builder.fillRectangle(with: properties, size: requestedSize.paddingZone)
+    
     if properties.padding.left != 0 || properties.padding.top != 0 {
         builder.pushPropagatingProperty(position: simd_float2(properties.padding.left, properties.padding.top))
     }
@@ -26,18 +27,22 @@ private func renderViewProperties<V: View>(_ view: V, builder: GuiViewBuilder, p
 }
 
 @MainActor
-func renderTree<V: View>(_ view: V, builder: GuiViewBuilder, maxWidth: Float, maxHeight: Float) -> SizeInformation {
-    if let anyView = view as? AnyView {
-        return anyView.boxAction({ renderTree($0, builder: builder, maxWidth: maxWidth, maxHeight: maxHeight ) })
+private func renderOverlay(requestedSize: SizeInformation, builder: GuiViewBuilder, properties: ViewProperties) {
+    // we could lean on the renderViewProperties above and just pop the padding position but that feels frail
+    // and more resilient to set up the position stack again.
+    if let position = properties.position {
+        let translation = properties.positionTranslation
+        builder.pushPropagatingProperty(position: translation(position))
     }
-    
-    let startingStackSize = builder.getPropagatingPropertiesStackSize()
-    var properties = ViewProperties.getDefault()
-    if let propertyView = view as? any HasViewProperties {
-        properties = propertyView.properties
+    builder.pushPropagatingProperty(position: simd_float2(properties.margin.left, properties.margin.top))
+    builder.border(with: properties, size: requestedSize.paddingZone)
+    builder.popPropagatingProperty()
+    if let _ = properties.position {
+        builder.popPropagatingProperty()
     }
-    let requestedSize = renderViewProperties(view, builder: builder, properties: properties, maxWidth: maxWidth, maxHeight: maxHeight)
-    
+}
+
+@MainActor func renderView<V: View>(_ view: V, requestedSize: SizeInformation, properties: ViewProperties, builder: GuiViewBuilder) {
     if let panel = view as? Panel {
         builder.resetForChild()
         panel.children.forEach({ let _ = renderTree($0, builder: builder, maxWidth: requestedSize.contentZone.x, maxHeight: requestedSize.contentZone.y ) })
@@ -53,12 +58,27 @@ func renderTree<V: View>(_ view: V, builder: GuiViewBuilder, maxWidth: Float, ma
     else if let text = view as? Text {
         builder.text(text: text.content, properties: properties)
     }
+}
+
+@MainActor
+func renderTree<V: View>(_ view: V, builder: GuiViewBuilder, maxWidth: Float, maxHeight: Float) -> SizeInformation {
+    if let anyView = view as? AnyView {
+        return anyView.boxAction({ renderTree($0, builder: builder, maxWidth: maxWidth, maxHeight: maxHeight ) })
+    }
     
-    // overlay rendering
-    builder.border(with: properties, size: requestedSize.contentZone)
+    let startingStackSize = builder.getPropagatingPropertiesStackSize()
+    var properties = ViewProperties.getDefault()
+    if let propertyView = view as? any HasViewProperties {
+        properties = propertyView.properties
+    }
+    let requestedSize = renderViewProperties(view, builder: builder, properties: properties, maxWidth: maxWidth, maxHeight: maxHeight)
+    renderView(view, requestedSize: requestedSize, properties: properties, builder: builder)
     
     while (builder.getPropagatingPropertiesStackSize() > startingStackSize) {
         builder.popPropagatingProperty()
     }
+    
+    renderOverlay(requestedSize: requestedSize, builder: builder, properties: properties)
+    
     return requestedSize
 }
